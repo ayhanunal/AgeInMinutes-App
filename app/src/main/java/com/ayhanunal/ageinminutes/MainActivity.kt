c178bb5d8fa430b77e62580ec5ea7d4053693210
ayhanunal <unalayhan@yahoo.com>
2021-08-14 20:34:17 +0300

    adding a DatePickerDialog

--- app/src/main/java/com/ayhanunal/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/ageinminutes/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.ayhanunal.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker(it)
+        }
+
+
+
     }
+
+    fun clickDatePicker(view: View){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener {
+                    view, year, month, dayOfMonth ->
+
+                
+            }
+            ,year
+            ,month
+            ,day).show()
+    }
+
 }
